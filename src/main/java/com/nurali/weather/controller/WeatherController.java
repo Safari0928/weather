d42f5f1fccc811e1b71e166728c0394cae716853
Alli <nurali0928@gmail.com>
2024-03-07 14:29:24 +0300

    name change

--- src/main/java/com/nurali/weather/controller/WeatherController.java
+++ src/main/java/com/nurali/weather/controller/WeatherController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/weather")
+@RequestMapping("api/v1/weather")
 public class WeatherController {
 
     private final WeatherService weatherService;
